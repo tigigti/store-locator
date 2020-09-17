97131c35a3d77f61bf6df8322cab5957ea141c29
Angelos Ioannou <angelos.ioannou@gmx.de>
2020-09-17 05:35:03 +0200

    import csv and display marker on map

--- src/hereMap.js
+++ src/hereMap.js
@@ -1,18 +1,44 @@
 import H from "@here/maps-api-for-javascript/bin/mapsjs.bundle";
 import { hereMapKey } from "../keys";
+import csvData from "./Emma_Belgium_Stores.csv";
 
 // Initialize the platform object:
-var platform = new H.service.Platform({
+const platform = new H.service.Platform({
     apikey: hereMapKey,
 });
 
 // Obtain the default map types from the platform object
-var maptypes = platform.createDefaultLayers();
+const maptypes = platform.createDefaultLayers();
 
 // Instantiate (and display) a map object:
-var map = new H.Map(document.getElementById("map"), maptypes.vector.normal.map, {
+const map = new H.Map(document.getElementById("map"), maptypes.vector.normal.map, {
     zoom: 10,
-    center: { lng: 13.4, lat: 52.51 },
+    center: { lng: 4.36, lat: 50.84 },
 });
+
+const mapEvents = new H.mapevents.MapEvents(map);
+
+map.addEventListener("tap", (e) => {
+    console.log(e.type, e.currentPointer.type);
+});
+
+const behavior = new H.mapevents.Behavior(mapEvents);
+
+const coords = { lat: 50.84, lng: 4.36 };
+const marker = new H.map.Marker(coords);
+
+for (let store in csvData) {
+    // console.log("lat: ", csvData[store][7], "long: ", csvData[store][8]);
+    const coords = {
+        lat: csvData[store][7],
+        lng: csvData[store][8],
+    };
+    console.log(coords);
+
+    const marker = new H.map.Marker(coords);
+    map.addObject(marker);
+}
+
+map.addObject(marker);
 
 // console.log(map);
