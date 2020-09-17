5519ec7085b08624d9efc784154825186d417395
Angelos Ioannou <angelos.ioannou@gmx.de>
2020-09-17 21:38:06 +0200

    clustering

--- src/hereMap.js
+++ src/hereMap.js
@@ -2,8 +2,65 @@ import H from "@here/maps-api-for-javascript/bin/mapsjs.bundle";
 import { hereMapKey } from "../keys";
 import csvData from "./Emma_Belgium_Stores.csv";
 
+function startClustering(map, data) {
+    // First we need to create an array of DataPoint objects,
+    // for the ClusterProvider
+    var dataPoints = data.map(function (item) {
+        // Check filter
+        return new H.clustering.DataPoint(item.lat, item.lng, null, item);
+    });
+
+    // Create a clustering provider with custom options for clusterizing the input
+    var clusteredDataProvider = new H.clustering.Provider(dataPoints, {
+        clusteringOptions: {
+            // Maximum radius of the neighbourhood
+            eps: 32,
+            // minimum weight of points required to form a cluster
+            minWeight: 2,
+        },
+    });
+
+    const defaultTheme = clusteredDataProvider.getTheme();
+
+    const customTheme = {
+        getClusterPresentation: function (cluster) {
+            //Keep the default theme for clusters
+            // const data = cluster.getData();
+            const clusterMarker = defaultTheme.getClusterPresentation.call(defaultTheme, cluster);
+
+            // clusterMarker.setData(data);
+            return clusterMarker;
+        },
+        getNoisePresentation: function (noisePoint) {
+            // Get a reference to data object our noise points
+            const data = noisePoint.getData();
+            // Create a marker for the noisePoint
+            const noiseMarker = new H.map.Marker(noisePoint.getPosition(), {
+                min: noisePoint.getMinZoom(),
+            });
+
+            noiseMarker.setData(data);
+            return noiseMarker;
+        },
+    };
+
+    clusteredDataProvider.setTheme(customTheme);
+
+    // Create a layer tha will consume objects from our clustering provider
+    var clusteringLayer = new H.map.layer.ObjectLayer(clusteredDataProvider);
+
+    // To make objects from clustering provder visible,
+    // we need to add our layer to the map
+    map.addLayer(clusteringLayer);
+
+    clusteredDataProvider.addEventListener("tap", (e) => {
+        console.log(e.target.getData());
+        map.setCenter(e.target.getGeometry());
+    });
+}
+
 // Initialize the platform object:
-const platform = new H.service.Platform({
+export const platform = new H.service.Platform({
     apikey: hereMapKey,
 });
 
@@ -11,34 +68,37 @@ const platform = new H.service.Platform({
 const maptypes = platform.createDefaultLayers();
 
 // Instantiate (and display) a map object:
-const map = new H.Map(document.getElementById("map"), maptypes.vector.normal.map, {
+export const map = new H.Map(document.getElementById("map"), maptypes.vector.normal.map, {
     zoom: 10,
     center: { lng: 4.36, lat: 50.84 },
 });
 
 const mapEvents = new H.mapevents.MapEvents(map);
-
-map.addEventListener("tap", (e) => {
-    console.log(e.type, e.currentPointer.type);
-});
-
 const behavior = new H.mapevents.Behavior(mapEvents);
 
-const coords = { lat: 50.84, lng: 4.36 };
-const marker = new H.map.Marker(coords);
+// for (let store in csvData) {
+//     const coords = {
+//         lat: csvData[store][7],
+//         lng: csvData[store][8],
+//     };
 
-for (let store in csvData) {
-    // console.log("lat: ", csvData[store][7], "long: ", csvData[store][8]);
-    const coords = {
-        lat: csvData[store][7],
-        lng: csvData[store][8],
+//     const marker = new H.map.Marker(coords);
+//     map.addObject(marker);
+// }
+
+const stores = [];
+
+for (let i = 1; i < csvData.length; i++) {
+    const store = {
+        lat: csvData[i][7],
+        lng: csvData[i][8],
+        name: csvData[i][0],
     };
-    console.log(coords);
 
-    const marker = new H.map.Marker(coords);
-    map.addObject(marker);
+    stores.push(store);
+
+    // const marker = new H.map.Marker(coords);
+    // map.addObject(marker);
 }
 
-map.addObject(marker);
-
-// console.log(map);
+startClustering(map, stores);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import "./style.scss";
 import "./hereMap";
+import { platform, map } from "./hereMap";
 
 const storeFinderForm = document.querySelector("#store-finder-form");
 
-storeFinderForm.addEventListener("submit", (e) => e.preventDefault());
+const service = platform.getSearchService();
+
+storeFinderForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // console.log(e.target.elements["location"].value);
+    const query = e.target.elements["location"].value;
+    service.geocode(
+        {
+            q: query,
+        },
+        (res) => {
+            res.items.forEach((item) => {
+                // map.addObject(new H.map.Marker(item.position));
+                map.setCenter(item.position);
+                map.setZoom(14);
+            });
+        }
+    );
+});
